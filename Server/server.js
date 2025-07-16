f1034f39248e65a0ac137e0c3644c53552a439b5
Ankit Rathor <ankitrathore48132@gmail.com>
2025-07-16 17:54:20 +0530

    add a mongoDB

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,26 @@
-const express = require("express")
-const router = require("./Routes/auth-router")
-require('dotenv').config()
-
-const app = express()
-
-
-app.use(router)
-const PORT = process.env.PORT || 9000
+require("dotenv").config();
+const express = require("express");
+const router = require("./Routes/auth-router");
+const {  mongoose } = require("mongoose");
+const  connectDb  = require("./utils/db");
 
 
+const app = express();
 
-app.listen( PORT ,()=>{
-    console.log(`server is runnig port ${PORT}`)
-}  )
+app.use(router);
+const PORT = process.env.PORT || 9000;
+
+// connectDb().then(()=>{
+//     console.log("hadifhsofishf")
+// })
+
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server is runnig  ${PORT}`);
+  });
+});
+
+
+//  app.listen(PORT, () => {
+//     console.log(`server is runnig  ${PORT}`);
+//   });
